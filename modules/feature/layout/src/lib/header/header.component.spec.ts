6913b20be0b9e00d5f33318fbf8e60fa159d1917
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-07-30 17:47:45 -0300

    feat(layout): add header

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
+// Mentoria Angular Pro
+// Por que testar o nosso código?
+// Garantir que o sistema funcione como deveria
+// Serve como documentação
+// Ajuda a criar componentes mais coesos (qualidade de código)
+// Confiança para alterar ou refatorar o código
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
@@ -17,5 +24,11 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain title', () => {
+    const header: HTMLHeadingElement =
+      fixture.nativeElement.querySelector('header');
+    expect(header.textContent).toBe('Ecommerce');
   });
 });
