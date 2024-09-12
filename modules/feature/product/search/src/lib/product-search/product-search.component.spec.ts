88ae330242099ff73fb94764204d5042a31bb76b
Maria Eduarda <>
2024-09-12 14:24:49 -0300

    feat(product-search): add search to header

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -1,8 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductSearchComponent } from './product-search.component';
 
@@ -12,14 +8,7 @@ describe('ProductSearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        ProductSearchComponent,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatAutocompleteModule,
-        NoopAnimationsModule,
-      ],
+      imports: [ProductSearchComponent, NoopAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductSearchComponent);
