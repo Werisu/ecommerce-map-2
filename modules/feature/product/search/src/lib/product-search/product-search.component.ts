71954bbbb3d11f535498331c4e99283dc376bf3b
Maria Eduarda <>
2024-09-12 15:49:14 -0300

    feat(product-search): integrates api

--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { mockProducts } from '@ecommerce-map-2/product-data-access';
+import { ProductSearchService } from '@ecommerce-map-2/product-data-access';
+import { Product } from 'modules/data-access/product/src/lib/models/product.model';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  Observable,
+  switchMap,
+} from 'rxjs';
 
 @Component({
   selector: 'lib-product-search',
@@ -19,7 +27,18 @@ import { mockProducts } from '@ecommerce-map-2/product-data-access';
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
 })
-export class ProductSearchComponent {
-  control = new FormControl('');
-  products = mockProducts;
+export class ProductSearchComponent implements OnInit {
+  control = new FormControl('', { nonNullable: true });
+  products$!: Observable<Product[]>;
+
+  constructor(private productSearchService: ProductSearchService) {}
+
+  ngOnInit() {
+    this.products$ = this.control.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      filter((text) => text.length > 2),
+      switchMap((text) => this.productSearchService.searchByName(text))
+    );
+  }
 }
