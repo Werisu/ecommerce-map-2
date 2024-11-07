3983741fd5c468e0d363296b2ecf07dbcac877c0
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-11-07 16:10:23 -0300

    feat(product-detail): get product id

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,22 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import {
+  NoPreloading,
+  provideRouter,
+  withComponentInputBinding,
+  withPreloading,
+} from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(
+      appRoutes,
+      withPreloading(NoPreloading),
+      withComponentInputBinding()
+    ),
     provideHttpClient(),
     provideAnimationsAsync(),
   ],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,11 @@ export const appRoutes: Route[] = [
     loadChildren: () =>
       import('@ecommerce-map-2/home').then((m) => m.homeRoutes),
   },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('@ecommerce-map-2/product-detail').then(
+        (m) => m.productDetailRoutes
+      ),
+  },
 ];
